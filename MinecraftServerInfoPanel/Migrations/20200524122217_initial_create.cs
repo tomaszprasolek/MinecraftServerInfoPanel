e8c240d2db4fc7cec566374a3c267de2611ae5c4
Tomasz Prasołek <prasol@o2.pl>
2020-05-24 14:23:37 +0200

    Add db migration

---
+++ MinecraftServerInfoPanel/Migrations/20200524122217_initial_create.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MinecraftServerInfoPanel.Migrations
+{
+    public partial class initial_create : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ConsoleLogs",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Date = table.Column<DateTime>(nullable: false),
+                    Information = table.Column<string>(nullable: true),
+                    IsNeededToSendEmail = table.Column<bool>(nullable: false),
+                    SendEmail = table.Column<bool>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ConsoleLogs", x => x.Id);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ConsoleLogs");
+        }
+    }
+}
