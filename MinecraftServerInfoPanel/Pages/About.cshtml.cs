9f5ca07e4724eae4f8871aa18365bcf6741c9f5f
Tomasz Prasołek <prasol@o2.pl>
2020-05-28 18:20:37 +0200

    Add 'About' page
    Show there application version

---
+++ MinecraftServerInfoPanel/Pages/About.cshtml.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc.RazorPages;
+using System.Reflection;
+
+namespace MinecraftServerInfoPanel.Pages
+{
+    public class AboutModel : PageModel
+    {
+        public string Version { get; set; }
+
+        public void OnGet()
+        {
+            Version = Assembly.GetEntryAssembly().GetCustomAttribute<AssemblyInformationalVersionAttribute>().InformationalVersion;
+        }
+    }
+}
