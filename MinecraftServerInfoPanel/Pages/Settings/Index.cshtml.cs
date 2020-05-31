c4fd50c82d4ca971342a9069d41552249389e8c2
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 11:15:11 +0200

    Add Authorize attribute to settings page

--- MinecraftServerInfoPanel/Pages/Settings/Index.cshtml.cs
+++ MinecraftServerInfoPanel/Pages/Settings/Index.cshtml.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using MinecraftServerInfoPanel.Database;
 using System.Collections.Generic;
@@ -6,6 +7,7 @@ using System.Linq;
 
 namespace MinecraftServerInfoPanel.Pages.Settings
 {
+    [Authorize]
     public class SettingsModel : PageModel
     {
         private readonly MinecraftDbContext dbContext;
