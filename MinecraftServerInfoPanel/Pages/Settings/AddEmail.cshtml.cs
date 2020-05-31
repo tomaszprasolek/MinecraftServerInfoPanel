59319bb780669e919ff3f9ac7500fef606f00280
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 07:26:46 +0200

    Add adding emails feature

---
+++ MinecraftServerInfoPanel/Pages/Settings/AddEmail.cshtml.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MinecraftServerInfoPanel.Database;
+using System.ComponentModel.DataAnnotations;
+
+namespace MinecraftServerInfoPanel.Pages.Settings
+{
+    public class AddEmailModel : PageModel
+    {
+        private readonly MinecraftDbContext dbContext;
+
+        [Required(ErrorMessage = "Nie podano poprawnego adresu e-mail")]
+        [DataType(DataType.EmailAddress)]
+        [Display(Name = "Email")]
+        [BindProperty]
+        public string NewEmail { get; set; }
+
+        public AddEmailModel(MinecraftDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public void OnGet()
+        {
+            
+        }
+
+        public IActionResult OnPost()
+        {
+            dbContext.Emails.Add(new Email { EmailAddress = NewEmail });
+            dbContext.SaveChanges();
+
+            TempData["Message"] = "Dodano nowy adres email";
+            return RedirectToPage("Index");
+        }
+    }
+}
