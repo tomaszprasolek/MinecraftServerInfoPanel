87b0ea2e0e66b74f3b1f5494b57c4a40144e7316
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 11:14:40 +0200

    Add sending test emails

---
+++ MinecraftServerInfoPanel/Pages/Settings/SendTestEmail.cshtml.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MinecraftServerInfoPanel.BL.EmailSender;
+using MinecraftServerInfoPanel.Database;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MinecraftServerInfoPanel.Pages.Settings
+{
+    public class SendTestEmailModel : PageModel
+    {
+        private readonly IEmailSender emailSender;
+        private readonly MinecraftDbContext dbContext;
+
+        public SendTestEmailModel(IEmailSender emailSender,
+            MinecraftDbContext dbContext)
+        {
+            this.emailSender = emailSender;
+            this.dbContext = dbContext;
+        }
+
+        public async Task<IActionResult> OnPost()
+        {
+            var emails = dbContext.Emails.Select(x => x.EmailAddress).ToList();
+
+            for (int i = 0; i < emails.Count; i++)
+            {
+                await emailSender.SendEmailAsync(emails[i], "Email testowy", "To jest email testowy. Proszę nie odpowiadać na niego");
+            }
+
+            TempData["Message"] = "Wysłano emaila testowego";
+            return RedirectToPage("Index");
+        }
+    }
+}
