a69c5c0b0438d7e95f7bddbcbd8b596c748e2cb7
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 11:19:34 +0200

    In debug put password to form

--- MinecraftServerInfoPanel/Pages/Account/Login.cshtml.cs
+++ MinecraftServerInfoPanel/Pages/Account/Login.cshtml.cs
@@ -1,8 +1,10 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
+using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Hosting;
 using System.ComponentModel.DataAnnotations;
 using System.Security.Claims;
 using System.Threading.Tasks;
@@ -12,15 +14,23 @@ namespace MinecraftServerInfoPanel.Pages.Account
     public class LoginModel : PageModel
     {
         private readonly IConfiguration configuration;
+        private readonly IWebHostEnvironment webHostEnvironment;
 
         [Required(ErrorMessage ="Hasło jest wymanage")]
         [Display(Name = "Hasło")]
         [BindProperty]
         public string Password { get; set; }
 
-        public LoginModel(IConfiguration configuration)
+        public LoginModel(IConfiguration configuration, IWebHostEnvironment webHostEnvironment)
         {
             this.configuration = configuration;
+            this.webHostEnvironment = webHostEnvironment;
+        }
+
+        public void OnGet()
+        {
+            if (webHostEnvironment.IsDevelopment())
+                Password = configuration["Password"];
         }
 
         public async Task<IActionResult> OnPostAsync()
