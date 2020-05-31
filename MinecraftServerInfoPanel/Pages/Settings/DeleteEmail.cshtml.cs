194e899e9c913b86a17a29984a882097005d4047
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 11:05:37 +0200

    Add removing emails feature

---
+++ MinecraftServerInfoPanel/Pages/Settings/DeleteEmail.cshtml.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using MinecraftServerInfoPanel.Database;
+using System.Linq;
+
+namespace MinecraftServerInfoPanel.Pages.Settings
+{
+    public class DeleteEmailModel : PageModel
+    {
+        private readonly MinecraftDbContext dbContext;
+
+        public Email Email { get; set; }
+
+        public DeleteEmailModel(MinecraftDbContext dbContext)
+        {
+            this.dbContext = dbContext;
+        }
+
+        public IActionResult OnGet(int id)
+        {
+            Email = dbContext.Emails.Where(x => x.Id == id).FirstOrDefault();
+            if (Email == null)
+            {
+                return RedirectToPage("./NotFound");
+            }
+            return Page();
+        }
+
+        public IActionResult OnPost(int id)
+        {
+            var email = dbContext.Emails.Where(x => x.Id == id).FirstOrDefault();
+            if (email != null)
+            {
+                dbContext.Remove(email);
+                dbContext.SaveChanges();
+
+                TempData["Message"] = $"Adres {email.EmailAddress} został usunięty.";
+            }
+
+            return RedirectToPage("Index");
+        }
+    }
+}
