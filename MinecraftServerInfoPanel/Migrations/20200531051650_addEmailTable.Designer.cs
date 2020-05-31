c7a1b20c46940b0d5c75781d6a09a55e4a59c32a
Tomasz Prasołek <prasol@o2.pl>
2020-05-31 07:18:55 +0200

    Add migration, add Email table

--- MinecraftServerInfoPanel/Migrations/20200531051650_addEmailTable.Designer.cs
+++ MinecraftServerInfoPanel/Migrations/20200531051650_addEmailTable.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MinecraftServerInfoPanel.Database;
 
 namespace MinecraftServerInfoPanel.Migrations
 {
     [DbContext(typeof(MinecraftDbContext))]
-    partial class MinecraftDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200531051650_addEmailTable")]
+    partial class addEmailTable
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
