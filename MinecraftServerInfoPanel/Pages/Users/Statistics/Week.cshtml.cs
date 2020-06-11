ebce1a948805cf68bea2d7092d2b2c263bcff02b
Tomasz Prasołek <prasol@o2.pl>
2020-06-11 12:50:12 +0200

    Add navigation to previous and next week

--- MinecraftServerInfoPanel/Pages/Users/Statistics/Week.cshtml.cs
+++ MinecraftServerInfoPanel/Pages/Users/Statistics/Week.cshtml.cs
@@ -14,6 +14,8 @@ namespace MinecraftServerInfoPanel.Pages.Users.Statistics
         private readonly MinecraftDbContext dbContext;
         private readonly IPlayTimeCalculator playTimeCalculator;
 
+        private Week selectedWeek;
+
         public List<UserDayStatisticsViewmodel> ViewModel { get; set; }
 
         [BindProperty(SupportsGet = true)]
@@ -22,8 +24,8 @@ namespace MinecraftServerInfoPanel.Pages.Users.Statistics
         [BindProperty(SupportsGet = true)]
         public TimePeriod Period { get; set; }
 
-        //public string PreviousDay => Date.AddDays(-1).ToString("yyyy-MM-dd");
-        //public string NextDay => Date.AddDays(1).ToString("yyyy-MM-dd");
+        public string PreviousWeek => selectedWeek.GetPreviousWeek().ToString();
+        public string NextWeek => selectedWeek.GetNextWeek().ToString();
 
         public WeekModel(MinecraftDbContext dbContext, IPlayTimeCalculator playTimeCalculator)
         {
@@ -37,24 +39,69 @@ namespace MinecraftServerInfoPanel.Pages.Users.Statistics
                 .Select(x => new { x.Id, x.UserName })
                 .ToList();
 
-            DateTime date;
-            if (string.IsNullOrWhiteSpace(Week) == false)
-            {
-                var result = Week.Split("-W");
-                date = ISOWeek.ToDateTime(Convert.ToInt32(result[0]), Convert.ToInt32(result[1]), DayOfWeek.Monday);
-            }
-            else
-                date = DateTime.Now;
+            selectedWeek = Statistics.Week.ToWeek(Week);
 
             ViewModel = users.Select(x => new UserDayStatisticsViewmodel
             {
                 UserName = x.UserName,
-                PlayTime = playTimeCalculator.CalculateUserPlayTime(x.UserName, Period, date)
+                PlayTime = playTimeCalculator.CalculateUserPlayTime(x.UserName, Period, selectedWeek.GetFirstDayOfWeek())
             })
             .OrderByDescending(x => x.PlayTime)
             .ToList();
 
             return Page();
+        }
+    }
+
+    public class Week
+    {
+        public readonly int Year;
+        public readonly int WeekNr;
+
+        public Week(int year, int week)
+        {
+            Year = year;
+            WeekNr = week;
+        }
+
+        public static Week ToWeek(string isoWeek)
+        {
+            if (string.IsNullOrWhiteSpace(isoWeek))
+                return new Week(DateTime.Now.Year, ISOWeek.GetWeekOfYear(DateTime.Now));
+
+            var result = isoWeek.Split("-W");
+            return new Week(Convert.ToInt32(result[0]), Convert.ToInt32(result[1]));
+        }
+
+        public DateTime GetFirstDayOfWeek()
+        {
+            return ISOWeek.ToDateTime(Year, WeekNr, DayOfWeek.Monday);
+        }
+
+        public Week GetNextWeek()
+        {
+            int weeksInyear = ISOWeek.GetWeeksInYear(Year);
+            int nextWeekNr = WeekNr + 1;
+
+            if (nextWeekNr > weeksInyear)
+                return new Week(Year + 1, 1);
+
+            return new Week(Year, nextWeekNr);
+        }
+
+        public Week GetPreviousWeek()
+        {
+            if (WeekNr == 1)
+            {
+                return new Week(Year - 1, ISOWeek.GetWeeksInYear(Year - 1));
+            }
+
+            return new Week(Year, WeekNr - 1);
+        }
+
+        public override string ToString()
+        {
+            return $"{Year}-W{WeekNr}";
         }
     }
 }
