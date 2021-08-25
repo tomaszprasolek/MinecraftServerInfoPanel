0d0b3064535e4e0fcb20643be8a69f8d2d67cacc
Tomasz Prasołek <prasol@o2.pl>
2021-08-25 05:40:48 +0200

    wip

---
+++ MinecraftServerInfoPanel.Tests/CalculateUserPlayTimeShould.cs
@@ -0,0 +1,33 @@
+using System;
+using Xunit;
+
+namespace MinecraftServerInfoPanel.Tests
+{
+    public class CalculateUserPlayTimeShould
+    {
+        [Fact]
+        public void calculate_time_for_day_period()
+        {
+
+        }
+
+
+        [Fact]
+        public void calculate_time_for_week_period()
+        {
+
+        }
+
+        [Fact]
+        public void calculate_time_for_month_period()
+        {
+
+        }
+
+        [Fact]
+        public void calculate_time_for_day_alltime()
+        {
+
+        }
+    }
+}
