7b0861e107a91829ddd0708e48ad527e1ce3324b
Tomasz Prasołek <tomasz.prasolek@gmail.com>
2021-08-25 18:54:51 +0200

    Prepare project for unit tests, add first unit tests

---
+++ tests/MinecraftServerInfoPanel.UnitTests/CalculateUserAllPlayTimeTests.cs
@@ -0,0 +1,71 @@
+﻿using FluentAssertions;
+using Microsoft.EntityFrameworkCore;
+using MinecraftServerInfoPanel.BL.PlayTimeCalculator;
+using MinecraftServerInfoPanel.DataLayer;
+using MinecraftServerInfoPanel.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using Xunit;
+
+namespace MinecraftServerInfoPanel.UnitTests
+{
+    public class CalculateUserAllPlayTimeTests
+    {
+        private DbContextOptions<MinecraftDbContext> dbContextOptions = new DbContextOptionsBuilder<MinecraftDbContext>()
+            .UseInMemoryDatabase(databaseName: "MinecraftDb")
+            .Options;
+
+        private MinecraftDbContext dbContext;
+
+        public CalculateUserAllPlayTimeTests()
+        {
+            if (dbContext != null)
+                dbContext.Dispose();
+            dbContext = new MinecraftDbContext(dbContextOptions);
+            //SeedDb();
+        }
+
+        [Fact]
+        public void no_data_in_db_return_zero()
+        {
+            // Arrange
+
+            // Act
+            var result = Execute();
+
+            // Assert
+            result.Should().Be(TimeSpan.Zero);
+        }
+
+        [Fact]
+        public void alltime()
+        {
+            // Arrange
+            SeedDb();
+
+            // Act
+            var result = Execute();
+
+            // Assert
+            result.Should().Be(TimeSpan.FromMinutes(58));
+        }
+
+        private TimeSpan Execute()
+        {
+            var sut = new PlayTimeCalculator(dbContext);
+            return sut.CalculateUserAllPlayTime("prasol");
+        }
+
+        private void SeedDb()
+        {
+            List<DbConsoleLog> logs = new List<DbConsoleLog>
+            {
+                new DbConsoleLog { Id = 1, Date = new DateTime(2021, 8, 25, 18, 57, 33), Information = "prasol connected" },
+                new DbConsoleLog { Id = 2, Date = new DateTime(2021, 8, 25, 19, 55, 33), Information = "prasol disconnected" },
+            };
+
+            dbContext.AddRange(logs);
+            dbContext.SaveChanges();
+        }
+    }
+}
